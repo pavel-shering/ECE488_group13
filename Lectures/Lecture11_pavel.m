057399d277c3057c8c508584ad4c41a2d9eae119
henriettao <henriettaodiete@rocketmail.com>
2018-04-08 12:23:04 -0400

    Update Lecture11_pavel.m

--- Lectures/Lecture11_pavel.m
+++ Lectures/Lecture11_pavel.m
@@ -1,6 +1,6 @@
 close all; clear all; clc 
 
-%Student Numbers
+% Student Numbers
 % 20523043 Pavel 
 % 20509464 Tyler
 % 20482444 Henrietta
@@ -11,20 +11,20 @@ s = tf('s');
 P = 100 / ( (s+1)*(s-2)*(s+4) );
 P_prime = 100 / ( s* (s+1)*(s-2)*(s+4) );
 
-%find controller C(s) that stabilizes it
+% find controller C(s) that stabilizes it
 % plot step response for 10 seconds
 
-%step 1 - get A,B,C,D for the plant
+% step 1 - get A,B,C,D for the plant
 [A, B, C, D] = ssdata(P_prime);
 
-%step 2 - obbtain k - controllability?
+% step 2 - obtain k - controllability?
 p = [-1 -2 -30 -40]
 k = place (A, B, p);
 
-%check stability
+% check stability
 eig_vals = eig(A-B*k)
 
-%step 3 - obtain f - observability?
+% step 3 - obtain f - observability?
 p2 = [-50,-60,-700, -800];
 f = place(A', C', p2)
 f = f'
@@ -32,7 +32,7 @@ f = f'
 % check if stable 
 eig_vals2 = eig(A-f*C)
 
-%step 4 - get the xp, yp, dp, np 
+% step 4 - get the xp, yp, dp, np 
 [xp_n xp_d] = ss2tf(A-f*C, -f, -k, 0);
 [np_n np_d] = ss2tf(A-B*k, B, C-D*k, D);
 [yp_n yp_d] = ss2tf(A-f*C, -B+f*D, -k, 1);
